426d719dee87a708cb4946725acba8f26085a2c7
Dementexo <browndon962@gmail.com>
2023-01-04 16:09:35 -0600

    ~70% completion until phase 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 
 //Main body application, components can be found in comps.js.
-//Nodes are stylized by a mixture of Tailwind and Vanilla CSS.
+//Elements are stylized by a mixture of Tailwind and Vanilla CSS.
 
 function App() {
   return (
     <body className="App flex flex-col">
       <nav className="flex w-full h-16">
         {/* Navigation Bar Content */}
-        <h1 className="my-auto w-24 font-thin text-center text-white"><span className="font-normal text-white">DFB</span>Dev</h1>
-        <ul className="navOpts flex justify-center justify-between items-center max-w-xlg w-3/5 ml-auto mr-5 font-thin text-white">
-          <li className="">About me</li>
-          <li className="">Skills</li>
-          <li className="">Projects</li>
-          <li className="">Contact</li>
+        <h1 className="my-auto w-24 font-thin text-center text-white text-xl"><span className="font-normal text-white">DFB</span>Dev</h1>
+        <ul className="navOpts flex justify-center justify-between items-center max-w-xlg w-3/5 ml-auto mr-5 font-thin text-white sm:text-xl">
+          <li className="opt">About me</li>
+          <li className="opt">Skills</li>
+          <li className="opt">Projects</li>
+          <li className="opt">Contact</li>
         </ul>
       </nav>
       <section className="section1 flex w-screen">
         {/* Introduction Section */}
-        <figure className="flex w-3/4"><img className="personalImg mt-auto" src={require("./Images/Personal.png")}/></figure>
+        <figure className="flex w-3/4"><img className="personalImg mt-auto mx-auto" src={require("./Images/Personal.png")}/></figure>
         <article className="flex w-3/4 justify-center items-center flex-col">
           <p className="introText">
             <span className="font-thin">Hello! My name is...</span>
@@ -35,10 +35,10 @@ function App() {
       <section className="section2 flex flex-col justify-center w-screen">
         {/* Skills Section */}
         <header className="skillsHeader flex flex-col text-white my-5">
-          <span className="font-normal text-xl">Skills/Expertise</span>
-          <span className="font-thin">Photo Credits</span>
+          <span className="font-normal md:text-xl">Skills/Expertise</span>
+          <span className="font-thin md:text-lg">Click for Photo Credits</span>
         </header>
-        <article className="flex flex-wrap justify-center justify-around mb-5">
+        <article className="ssHolder flex flex-wrap justify-center items-center justify-around mb-5">
           <div className="skillSlot">
             <figure><img className="ssImg" src={require("./Images/html.png")}></img></figure>
             <figcaption className="ssCap">HTML</figcaption>
@@ -73,6 +73,42 @@ function App() {
           </div>
         </article>
       </section>
+      <section className="flex justify-center items-center">
+        {/* Projects Section */}
+        <article className="psContainer w-full flex">
+        <div className="projSec">
+          <header className="projHead md:text-xl text-sm font-normal">Recent Projects</header>
+          <div className="projHolderUno mr-auto">
+            <figure className="piHolder"><a className="piHolder2" href="https://dfbdev.github.io/Messaging-App"><img className="projImg" src={require("./Images/ss1.png")}></img></a></figure>
+          </div>
+        </div>
+        <div className="projSec pt-6">
+          <div className="projHolder mb-auto">
+            <figure className="piHolder"><a className="piHolder2" href="https://dfbdev.github.io/SignUpForm"><img className="projImg" src={require("./Images/ss2.png")}></img></a></figure>
+          </div>
+          <div className="projTitleHolder bg-orange-100 mt-5 rounded-tl-lg rounded-tr-lg">
+            <div className="projTitle border-solid border-r-violet-600 border-r-2">Messaging<br></br>App</div>
+            <div className="projTitle">Calculator</div>
+          </div>
+        </div>
+        <div className="projSec pb-6">
+          <div className="projTitleHolder bg-orange-100 mb-auto rounded-bl-lg rounded-br-lg">
+            <div className="projTitle border-solid border-r-violet-600 border-r-2">SignUp<br></br>Form</div>
+            <div className="projTitle">Emissary<br></br>Connect</div>
+          </div>
+          <div className="projHolder mt-3">
+            <figure className="piHolder"><a className="piHolder2" href="https://dfbdev.github.io/React-Calculator"><img className="projImg" src={require("./Images/ss3.png")}></img></a></figure>
+          </div>
+        </div>
+        <div className="projSec pt-3">
+          <div className="projHolder mb-auto mt-3">
+              <figure className="piHolder"><img className="projImg"></img></figure>
+          </div>
+        </div>
+        </article>
+      </section>
+      <section className="aboutSec flex justify-center items-center text-white">About Me section</section>
+      <footer className="flex justify-center items-center py-3 text-sm">Developed/Designed by © DFBDev</footer>
     </body>
   );
 }
